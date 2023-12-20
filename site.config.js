dc8189a9be112a68bdaabe1f0d489ca3cd0b558f
Lucas <38710249+lucas-nobrega@users.noreply.github.com>
2023-12-20 11:41:25 -0300

    Update site.config.js
    change link

--- site.config.js
+++ site.config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://soomi-blog.vercel.app/",
+  link: "https://blog.soomi.com.br/",
   since: 2023, // If leave this empty, current year will be used.
   lang: "pt-BR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
